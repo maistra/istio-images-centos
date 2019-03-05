787614cf3c685509de0e7c322b36aa58484f7a0d
Brian Avery <bavery@redhat.com>
2019-03-05 09:58:57 -0500

    Fixed spacing and default_images

--- create-images.sh
+++ create-images.sh
@@ -1,64 +1,64 @@
 #!/bin/bash
 
 function usage() {
-  [[ -n "${1}" ]] && echo "${1}"
+	[[ -n "${1}" ]] && echo "${1}"
 
-  cat <<EOF
+	cat <<EOF
 Usage: ${BASH_SOURCE[0]} [options ...]"
-  options:
-    -t <TAG>  TAG to use for operations on images, required.
-    -h <HUB>  Docker hub + username. Defaults to "docker.io/maistra"
-    -i <IMAGES> Specify which images should be built
-    -b        Build images
-    -d        Delete images
-    -p        Push images
+	options:
+		-t <TAG>  TAG to use for operations on images, required.
+		-h <HUB>  Docker hub + username. Defaults to "docker.io/maistra"
+		-i <IMAGES> Specify which images should be built
+		-b        Build images
+		-d        Delete images
+		-p        Push images
 
 At least one of -b, -d or -p is required.
 EOF
-  exit 2
+exit 2
 }
 
 HUB="docker.io/maistra"
-DEFAULT_IMAGES="citadel pilot mixer sidecar-injector proxy-init galley ior"
+DEFAULT_IMAGES="citadel pilot mixer sidecar-injector proxy-init galley istio-operator proxyv2 ior"
 IMAGES=${ISTIO_IMAGES:-$DEFAULT_IMAGES}
 
 
 CONTAINER_CLI=docker
 
 while getopts ":t:h:i:bdp" opt; do
-  case ${opt} in
-    t) TAG="${OPTARG}";;
-    h) HUB="${OPTARG}";;
-    b) BUILD=true;;
-    d) DELETE=true;;
-    p) PUSH=true;;
-    i) IMAGES="${OPTARG}";;
-    *) usage;;
-  esac
+	case ${opt} in
+		t) TAG="${OPTARG}";;
+		h) HUB="${OPTARG}";;
+		b) BUILD=true;;
+		d) DELETE=true;;
+		p) PUSH=true;;
+		i) IMAGES="${OPTARG}";;
+		*) usage;;
+	esac
 done
 
 [[ -z "${TAG}" ]] && usage "Missing TAG"
 [[ -z "${BUILD}" && -z "${DELETE}" && -z "${PUSH}" ]] && usage
 
 if [ -n "${DELETE}" ]; then
-  for image in ${IMAGES}; do
-    echo "Deleting image ${image}..."
+	for image in ${IMAGES}; do
+		echo "Deleting image ${image}..."
 		${CONTAINER_CLI} rmi ${HUB}/${image}-centos7:${TAG}
-  done
+	done
 fi
 
 if [ -n "${BUILD}" ]; then
-  for image in ${IMAGES}; do
-    echo "Building ${image}..."
-	  ${CONTAINER_CLI} build --no-cache -t ${HUB}/${image}-centos7:${TAG} -f Dockerfile.${image} .
-    echo "Done"
-    echo
-  done
+	for image in ${IMAGES}; do
+		echo "Building ${image}..."
+		${CONTAINER_CLI} build --no-cache -t ${HUB}/${image}-centos7:${TAG} -f Dockerfile.${image} .
+		echo "Done"
+		echo
+	done
 fi
 
 if [ -n "${PUSH}" ]; then
-  for image in ${IMAGES}; do
-    echo "Pushing image ${image}..."
-	${CONTAINER_CLI} push ${HUB}/${image}-centos7:${TAG}
-  done
+	for image in ${IMAGES}; do
+		echo "Pushing image ${image}..."
+		${CONTAINER_CLI} push ${HUB}/${image}-centos7:${TAG}
+	done
 fi
